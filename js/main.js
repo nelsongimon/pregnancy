e3db80280153b13d8c69dfcbe9240928b3b9a5da
Nelson Gimón <nelson.gimon@gmail.com>
2018-07-28 22:07:05 -0400

    commit 51

--- js/main.js
+++ js/main.js
@@ -70,3 +70,28 @@ $(document).ready(function () {
         })
     }
 });
+
+$(function(){
+    $('.carousel').carousel({
+        interval: 5000,
+        pause: false
+    });
+
+    //WOW Scroll Spy
+    var wow = new WOW({
+    //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+
+    var offset = 215;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('#navbar-menu').css('display', 'none');
+            $('#navbar-top').css('display', 'block');
+        } else {
+            $('#navbar-menu').css('display', 'block');
+            $('#navbar-top').css('display', 'none');
+        }
+    });
+})
